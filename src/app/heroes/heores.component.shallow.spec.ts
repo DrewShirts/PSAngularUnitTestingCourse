7036afab25f68daf445c607d02b0d7f52b855714
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-30 09:32:54 -0700

    Dealing with Lists of Elements

--- src/app/heroes/heores.component.shallow.spec.ts
+++ src/app/heroes/heores.component.shallow.spec.ts
@@ -1,5 +1,6 @@
 import {Component, Input, NO_ERRORS_SCHEMA} from "@angular/core";
 import {ComponentFixture, TestBed} from "@angular/core/testing"
+import {By} from "@angular/platform-browser";
 import {of} from "rxjs";
 import {Hero} from "../hero";
 import {HeroService} from "../hero.service";
@@ -45,6 +46,13 @@ describe('HeroesComponent (shallow tests)', () => {
     fixture.detectChanges();
 
     expect(fixture.componentInstance.heroes.length).toBe(3);
+  });
+
+  it('should create one li for each hero', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+    fixture.detectChanges();
+
+    expect(fixture.debugElement.queryAll(By.css('li')).length).toBe(3);
   });
 
 });
