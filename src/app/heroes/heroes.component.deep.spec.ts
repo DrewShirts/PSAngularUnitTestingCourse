a75f2528e06f84cc848584a2758be2f9e2d48610
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-30 11:00:47 -0700

    Interacting with Input Boxes

--- src/app/heroes/heroes.component.deep.spec.ts
+++ src/app/heroes/heroes.component.deep.spec.ts
@@ -59,4 +59,21 @@ describe('HeroesComponent (deep tests)', () => {
       expect(fixture.componentInstance.delete).toHaveBeenCalledWith(HEROES[0]);
   });
 
+  it('should add a new hero to the hero list when the add button is clicked', () => {
+    mockHeroService.getHeroes.and.returnValue(of(HEROES));
+
+    fixture.detectChanges();
+    const name = "Mr. Ice";
+    mockHeroService.addHero.and.returnValue(of({id: 5, name: name, strength: 4}));
+    const inputElement = fixture.debugElement.query(By.css('input')).nativeElement;
+    const addButton = fixture.debugElement.queryAll(By.css('button'))[0];
+
+    inputElement.value = name;
+    addButton.triggerEventHandler('click', null);
+    fixture.detectChanges();
+
+    const heroText = fixture.debugElement.query(By.css('ul')).nativeElement.textContent;
+    expect(heroText).toContain(name);
+  });
+
 });
