494baa44a2ade0767f214839a181fe193f6b9222
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-29 21:57:51 -0700

    Writing Your First Unit Test

---
+++ src/app/first-test.spec.ts
@@ -0,0 +1,17 @@
+describe('my first test', () => {
+  let sut;
+
+  beforeEach(() => {
+    sut = {};
+  });
+
+  it('should be true if true', () => {
+    // arrange
+    sut.a = false;
+
+    // act
+    sut.a = true;
+
+    expect(sut.a).toBe(true);
+  })
+})
