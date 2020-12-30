102a2952ada7d5e3d86606cf6afda9d59a2d3143
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-30 10:35:11 -0700

    Implementing a Test with Mocked HTTP

--- src/app/hero.service.spec.ts
+++ src/app/hero.service.spec.ts
@@ -1,11 +1,12 @@
 import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
-import {TestBed} from "@angular/core/testing";
+import {inject, TestBed} from "@angular/core/testing";
 import {HeroService} from "./hero.service";
 import {MessageService} from "./message.service";
 
 describe('HeroService', () => {
    let mockMessageService;
    let httpTestingController: HttpTestingController;
+   let service: HeroService;
 
    beforeEach(() => {
     mockMessageService = jasmine.createSpyObj(['add']);
@@ -19,5 +20,18 @@ describe('HeroService', () => {
     });
 
     httpTestingController = TestBed.get(HttpTestingController);
+    service = TestBed.get(HeroService);
+   });
+
+   describe('getHero', () => {
+
+    it('should call get with the correct URL', ()  => {
+
+      service.getHero(4).subscribe();
+
+      const req = httpTestingController.expectOne('api/heroes/4');
+      req.flush({id: 4, name: 'SuperDude', strength: 100});
+      httpTestingController.verify();
+    });
    });
 });
