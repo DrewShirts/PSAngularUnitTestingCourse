a0303ac0c00f57dcc5b463a3c2f8347b10bf0f09
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-30 08:59:49 -0700

    The TestBed

---
+++ src/app/hero/hero.component.shallow.spec.ts
@@ -0,0 +1,20 @@
+import {ComponentFixture, TestBed} from "@angular/core/testing"
+import {HeroesComponent} from "../heroes/heroes.component";
+import {HeroComponent} from "./hero.component"
+
+describe('HeroComponent (shallow tests)', () => {
+  let fixture: ComponentFixture<HeroComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [HeroComponent]
+    });
+    fixture = TestBed.createComponent(HeroComponent);
+  });
+
+  it('should have the correct hero', () => {
+    fixture.componentInstance.hero = { id: 1, name: 'SuperDude', strength: 3 };
+
+    expect(fixture.componentInstance.hero.name).toEqual('SuperDude');
+  })
+})
